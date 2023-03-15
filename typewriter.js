f9295a09e59acfc772d09add985c3e4c9349317a
Omar Hamid <omarhamid664@gmail.com>
2023-03-14 18:59:03 -0600

    run eslint on typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,16 @@
 const sentence = "hello there from lighthouse labs";
 
-
 const  printDelayed = (sentence) => {
   let timer = 50;
-    for (let char of sentence) {
-      setTimeout(() => {
-        process.stdout.write(char);
-      }, timer);
-      timer += 50;
-    }
+  for (let char of sentence) {
     setTimeout(() => {
-      console.log();
+      process.stdout.write(char);
     }, timer);
-  };
+    timer += 50;
+  }
+  setTimeout(() => {
+    console.log();
+  }, timer);
+};
 
 printDelayed(sentence);
