3a50ba5c1a62a08b5954da261e604737979b0533
Omar Hamid <omarhamid664@gmail.com>
2023-03-14 18:57:13 -0600

    implemented typewritter.js

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs";
+
+
+const  printDelayed = (sentence) => {
+  let timer = 50;
+    for (let char of sentence) {
+      setTimeout(() => {
+        process.stdout.write(char);
+      }, timer);
+      timer += 50;
+    }
+    setTimeout(() => {
+      console.log();
+    }, timer);
+  };
+
+printDelayed(sentence);
